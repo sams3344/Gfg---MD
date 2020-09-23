0c20895577dbd321826adc419f1afa3770895354
Sam <68379863+sams3344@users.noreply.github.com>
2020-09-23 22:14:34 +0530

    Create Implement strstr .cpp
    https://practice.geeksforgeeks.org/problems/implement-strstr/1

---
+++ Implement strstr .cpp
@@ -0,0 +1,37 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+int strstr(string ,string);
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string a;
+        string b;
+        
+        cin>>a;
+        cin>>b;
+        
+        cout<<strstr(a,b)<<endl;
+    }
+}
+// } Driver Code Ends
+
+
+/* The function should return position where the target string 
+   matches the string str
+Your are required to complete this method */
+int strstr(string s, string x)
+{
+     //Your code here
+    size_t found = s.find(x); 
+	if (found != string::npos) 
+		return found;
+	else
+	    return -1;
+}
+
