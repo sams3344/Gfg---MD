459be603108fb4b585e66f15549525e14dd66424
Sam <68379863+sams3344@users.noreply.github.com>
2021-03-11 16:45:35 +0530

    Delete Number of paths .cpp

--- Number of paths .cpp
+++
@@ -1,9 +0,0 @@
- // If either given row number is first or given column 
-    // number is first 
-    if (m == 1 || n == 1) 
-        return 1; 
-  
-    // If diagonal movements are allowed then the last 
-    // addition is required. 
-    return numberOfPaths(m - 1, n) + numberOfPaths(m, n - 1); 
-    // + numberOfPaths(m-1, n-1);
